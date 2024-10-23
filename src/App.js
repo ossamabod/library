030058aa188796df029a60acd6ec81ab270bacc2
ossamabod <ossama.14.boughallad@gmail.com>
2024-10-23 19:04:43 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Components/navbar';
+import Sidebar from './Components/sidebar';
+import BlogSection11 from './Components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Sidebar/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Include all component files
+    './public/index.html',         // Include your public HTML file(s)
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
